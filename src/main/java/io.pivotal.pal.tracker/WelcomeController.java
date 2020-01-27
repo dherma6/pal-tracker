1f3a2e3624efdf835a90483df6ebc7b03798592d
pal_user <dclherman@yahoo.com>
2020-01-27 15:52:06 -0700

    updates

--- src/main/java/io.pivotal.pal.tracker/WelcomeController.java
+++ src/main/java/io.pivotal.pal.tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
 
+    private String hello;
+
+    public WelcomeController(@Value("${welcome.message}") String hello) {
+        this.hello = hello;
+    }
+
     @GetMapping("/")
     public String sayHello () {
-        return "hello";
+        return this.hello;
     }
 }
